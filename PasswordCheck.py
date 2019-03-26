c9a50dbd3e4c35ab4688a84eee674b339b4a45c8
Felix Jansson <felixjansson.96@gmail.com>
2019-03-26 20:14:23 +0100

    PasswordCheck: Removed disclaimer print

--- PasswordCheck.py
+++ PasswordCheck.py
@@ -46,17 +46,8 @@ def isPasswordLeaked(hashedUserPassword, requestResult):
     # No match.
     return False, 0
 
-# User is responsible to trust this software.
-def printDisclaimer():
-    msg0 = "\n##############\n# DISCLAIMER #\n##############\n"
-    msg  = " This SOFTWARE PRODUCT is provided by THE PROVIDER \"as is\" and \"with all faults.\" THE PROVIDER makes no representations or warranties of any kind concerning the safety, suitability, lack of viruses, inaccuracies, typographical errors, or other harmful components of this SOFTWARE PRODUCT. There are inherent dangers in the use of any software, and you are solely responsible for determining whether this SOFTWARE PRODUCT is compatible with your equipment and other software installed on your equipment. You are also solely responsible for the protection of your equipment and backup of your data, and THE PROVIDER will not be liable for any damages you may suffer in connection with using, modifying, or distributing this SOFTWARE PRODUCT."
-    print(msg0)
-    print(msg)
-    print("\n##############")
-
 def main():
     print('\nExit script with [Ctrl + C] or [Ctrl + D]')
-    printDisclaimer()
     signalHandler = SignalHandler()
     while(True):
         try:
